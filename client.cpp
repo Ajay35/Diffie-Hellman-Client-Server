f144b73557adeb40d6a09db1e9a595ac82894770
Ajay35 <ajayjadhav35@live.com>
2019-02-04 23:31:17 +0530

    commit

--- client.cpp
+++ client.cpp
@@ -22,6 +22,9 @@ int server_connect(char *);
 void communicate_with_server (int,ll);
 
 
+char encoding_scheme[]={' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',',','.','?','0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!'};
+
+
 typedef struct {
 int opcode; 
 int s_addr; 
@@ -34,11 +37,116 @@ typedef struct {
   char ID[MAX_SIZE];
   ll q;
   char password[MAX_SIZE];
-  int status;
+  char status[MAX_SIZE];
   char file[MAX_SIZE]; 
   int dummy;
 } Msg;
 
+typedef struct {
+  char opcode[MAX_SIZE]; 
+  char s_addr[MAX_SIZE]; 
+  char d_addr[MAX_SIZE]; 
+} EncHdr;
+
+typedef struct {
+  EncHdr hdr;
+  char buf[MAX_LEN];
+  char ID[MAX_SIZE];
+  char q[MAX_SIZE];
+  char password[MAX_SIZE];
+  char status[MAX_SIZE];
+  char file[MAX_SIZE]; 
+  char dummy[MAX_SIZE];
+} EncMsg;
+
+string encrypt_util(string,ll);
+EncMsg encrypt_msg(Msg,ll);
+string decrypt_util(string,ll);
+Msg decrypt_msg(EncMsg,ll);
+
+
+string encrypt_util(string input,ll caesar_key){
+  int n=input.size();
+  string s;
+  s.resize(n,' ');
+  for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<66;j++)
+		{
+			if(input[i]==encoding_scheme[j]){	
+				s[i]=encoding_scheme[(j+caesar_key)%66];
+				break;
+			}	
+		}	
+	}
+  cout<<"original:"<<input<<endl;
+  cout<<"encrypted:"<<s<<endl;
+	return s;
+}
+
+EncMsg encrypt_msg(Msg send_msg,ll caesar_key)
+{
+	EncMsg enc_msg;
+  if(send_msg.hdr.opcode==10){
+    string op_code=to_string(send_msg.hdr.opcode);
+    op_code=encrypt_util(op_code,caesar_key);
+    string userid(send_msg.ID);
+    userid=encrypt_util(userid,caesar_key);
+    string password(send_msg.password);
+    password=encrypt_util(password,caesar_key);
+    string prime=to_string(send_msg.q);
+    prime=encrypt_util(prime,caesar_key);
+    strcpy(enc_msg.hdr.opcode,op_code.c_str());
+    strcpy(enc_msg.ID,userid.c_str());
+    strcpy(enc_msg.password,password.c_str());
+    strcpy(enc_msg.q,prime.c_str());
+  }
+  else if(send_msg.hdr.opcode==30){
+    string op_code=to_string(send_msg.hdr.opcode);
+    op_code=encrypt_util(op_code,caesar_key);
+    string userid(send_msg.ID);
+    userid=encrypt_util(userid,caesar_key);
+    string password(send_msg.password);
+    password=encrypt_util(password,caesar_key);
+    strcpy(enc_msg.hdr.opcode,op_code.c_str());
+    strcpy(enc_msg.ID,userid.c_str());
+    strcpy(enc_msg.password,password.c_str());
+    }
+
+  else if(send_msg.hdr.opcode==50){
+
+  }
+	return enc_msg;
+}
+
+Msg decrypt_msg(EncMsg recv_msg,ll caesar_key){
+    Msg msg;
+    string temp(recv_msg.hdr.opcode);
+    string status(recv_msg.status);
+    cout<<recv_msg.status<<endl;
+    cout<<"encryped status:"<<status<<endl;
+    msg.hdr.opcode=atoi(decrypt_util(temp,caesar_key).c_str());
+    strcpy(msg.status,decrypt_util(status,caesar_key).c_str());
+    return msg;
+}
+
+string decrypt_util(string input,ll caesar_key){
+  int n=input.size();
+  string s;
+  s.resize(n,' ');
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<66;j++)
+		{
+			if(input[i]==encoding_scheme[j])
+			{	
+				s[i]=encoding_scheme[(j - caesar_key + 66) % 66];
+				break;
+			}	
+		}
+	}
+	return s;
+}
 
 
 
@@ -70,53 +178,122 @@ int server_connect(char *sip)
 
 void communicate_with_server(int cfd,ll caesar_key){
    char buffer[MAX_LEN];
-   Msg send_msg;
-   Msg recv_msg;
-   int ch,status;
+   int ch,status,nbytes;
    while(true){
+
      cout<<"Choose operation:"<<endl;
      cout<<"1. Login Create"<<endl;
-     cout<<"2. Auth Request"<<endl;
-     cout<<"3. Service Request"<<endl;
+     cout<<"2. Service Request"<<endl;
      cin>>ch;
-     
-     switch(ch){
        
-     case 1:
-       Hdr header;
-       Msg send_msg,recv_msg;
-       string ID,password;
-       ll q;
-       header.opcode=10;
-       header.s_addr=1;
-       header.d_addr=2;
-       send_msg.Hdr=header;
-       cout<<"Enter user ID"<<endl;
-       cin>>str;
-       cout<<"Enter user password"<<endl;
-       cin>>password;
-       strcpy(send_msg.ID,ID.c_str());  
-       strcpy(send_msg.password,password.c_str());  
-       q=get_prime();
-       send_msg.q=q;
-       status=send(cfd, &send_msg,sizeof(Msg),0);
-       if (status == -1) {
-	 fprintf(stderr, "Client error: unable to send\n");
-	 return;
-       }
-       nbytes = recv(cfd, &recv_msg, sizeof(RepMsg), 0);
-       if (nbytes == -1) {
-	 fprintf(stderr, "Client error: unable to receive\n");
-       }
-       break;
-     case 2:
-       break;
-     case 3:
-       break;
-     default: exit(0);
+     if(ch== 1){
+        Msg send_msg,recv_msg;
+        string ID,password;
+        ll q;
+        send_msg.hdr.opcode=10;
+        send_msg.hdr.s_addr=1;
+        send_msg.hdr.d_addr=2;
+        cout<<"Enter user ID"<<endl;
+        cin>>ID;
+        cout<<"Enter user password"<<endl;
+        cin>>password;
+        strcpy(send_msg.ID,ID.c_str());  
+        strcpy(send_msg.password,password.c_str());  
+        q=get_prime();
+        send_msg.q=q;
 
+        EncMsg enc_msg=encrypt_msg(send_msg,caesar_key);
+        cout<<"encryped userid is:"<<enc_msg.ID<<endl;
+        status=send(cfd, &enc_msg,sizeof(EncMsg),0);
+        if (status==0) {
+            fprintf(stderr, "Client error: unable to send\n");
+            return;
+        }
      }
-   }
+    else if(ch==2){
+      cout<<"AUTH REQUEST"<<endl;
+        Msg send_msg,recv_msg;
+        string ID,password;
+        send_msg.hdr.opcode=30;
+        cout<<"Enter user ID"<<endl;
+        cin>>ID;
+        cout<<"Enter user password"<<endl;
+        cin>>password;
+        strcpy(send_msg.ID,ID.c_str());  
+        strcpy(send_msg.password,password.c_str());  
+        EncMsg enc_msg=encrypt_msg(send_msg,caesar_key);
+        status=send(cfd, &enc_msg,sizeof(enc_msg),0);
+        if (status <= 0) {
+            fprintf(stderr, "Client error: unable to send\n");
+            return;
+        }
+    }
+    else{
+      exit(0);
+    }
+
+    EncMsg recv_msg;
+    nbytes=recv(cfd, &recv_msg, sizeof(recv_msg), 0);
+    if (nbytes<=0){
+        fprintf(stderr, "Client error: unable to receive\n");
+        break;
+    }
+    cout<<"Data received in bytes:"<<nbytes<<endl;
+    
+    
+
+
+    /* decrypt here (according to header of msg)*/
+    Msg dec_recv_msg=decrypt_msg(recv_msg,caesar_key);
+
+
+    if(dec_recv_msg.hdr.opcode==20){
+      cout<<"LOGIN REPLY:"<<endl;
+      string test(dec_recv_msg.status);
+      if(test=="NO"){
+        cout<<"LOGIN FAILED"<<endl;
+      }
+      else if(test=="YES"){
+        cout<<"LOGIN SUCCESSFUL"<<endl;
+      }
+      else{
+        cout<<"INVALID RESPONSE"<<endl;
+      }
+    }  
+    else if(dec_recv_msg.hdr.opcode==40){
+      cout<<"AUTH REPLY:"<<endl;
+      if(dec_recv_msg.status==0){
+        cout<<"AUTH FAILED"<<endl;
+      }
+      else{
+        string file_name;
+        cout<<"AUTH SUCCESSFUL"<<endl;
+        /*Msg send_msg,recv_msg;
+        send_msg.hdr.opcode=50;
+        cout<<"Enter file name:"<<endl;
+        cin>>file_name;
+        strcpy(send_msg.file,file_name.c_str());
+        nbytes=send(cfd, &send_msg,sizeof(Msg),0);
+        if (nbytes == -1) {
+            fprintf(stderr, "Client error: unable to send\n");
+            return;
+        }
+
+        nbytes=recv(cfd, &recv_msg,sizeof(Msg),0);
+        if (nbytes == -1) {
+            fprintf(stderr, "Client error: unable to send\n");
+            return;
+        }
+
+        if(recv_msg.status==0){
+          cout<<"File Not found message"<<endl;
+        }
+        else{
+            cout<<"File Found"<<endl;
+        }*/
+      }    
+    }     
+  }
    
 
 }
@@ -155,8 +332,8 @@ int main(int argc, char* argv[]){
   int i=0,j=0;
   char temp[MAX_LEN];
   
-  while(cs_buf[i]!='\0'){
-    temp[j++]=cs_buf[i++];
+  while(sc_buf[i]!='\0'){
+    temp[j++]=sc_buf[i++];
   }    
   ll server_public_key=atoll(temp);
   
--- server.cpp
+++ server.cpp
@@ -6,7 +6,6 @@
 #include <string>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <time.h>
 #include <limits>
 #include <math.h>
 #include <random>
@@ -23,10 +22,13 @@ int start_server_connect();
 void server_loop(int);
 void comm_with_client(int,ll);
 
+char encoding_scheme[]={' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',',','.','?','0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!'};
+
+
 typedef struct {
-int opcode; /* opcode for a message */
-int s_addr; /* source address */
-int d_addr; /* destination address */
+int opcode; 
+int s_addr; 
+int d_addr; 
 } Hdr;
 
 typedef struct {
@@ -35,23 +37,129 @@ typedef struct {
   char ID[MAX_SIZE];
   ll q;
   char password[MAX_SIZE];
-  int status;
+  char status[MAX_SIZE];
   char file[MAX_SIZE]; 
   int dummy;
 } Msg;
+
+typedef struct{
+  string userid;
+  ll salt;
+  ll hash;
+  ll prime;
+}user;
+
+typedef struct {
+  char opcode[MAX_SIZE]; 
+  char s_addr[MAX_SIZE]; 
+  char d_addr[MAX_SIZE]; 
+} EncHdr;
+
+typedef struct {
+  EncHdr hdr;
+  char buf[MAX_LEN];
+  char ID[MAX_SIZE];
+  char q[MAX_SIZE];
+  char password[MAX_SIZE];
+  char status[MAX_SIZE];
+  char file[MAX_SIZE]; 
+  char dummy[MAX_SIZE];
+} EncMsg;
+
+Msg decrypt_msg(EncMsg,ll);
+string decrypt_util(string,ll);
+EncMsg encrypt_msg(Msg,ll);
+string encrypt_util(string,ll);
+
+vector<user> users;
+
+EncMsg encrypt_msg(Msg send_msg,ll caesar_key)
+{
+	EncMsg enc_msg; 
+  string op_code=to_string(send_msg.hdr.opcode);
+  op_code=encrypt_util(op_code,caesar_key);
+  string status(send_msg.status);
+  status=encrypt_util(status,caesar_key);
+  strcpy(enc_msg.hdr.opcode,op_code.c_str());
+  strcpy(enc_msg.status,status.c_str());
+  return enc_msg;
+}
+
+
+string encrypt_util(string input,ll caesar_key){
+  cout<<input<<endl;
+  int n=input.size();
+  string s;
+  s.resize(n,' ');
+  for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<66;j++)
+		{
+			if(input[i]==encoding_scheme[j])
+			{	
+				s[i]=encoding_scheme[(j + caesar_key) % 66];
+				break;
+			}	
+		}	
+	}
+	return s;
+}
+
+
+
+Msg decrypt_msg(EncMsg enc_msg,ll caesar_key){
+
+  Msg dec_msg;
+  string opcode(enc_msg.hdr.opcode);
+  string userid(enc_msg.ID);
+  string password(enc_msg.password);
+  string q(enc_msg.q);
+  int op_code=atoi(decrypt_util(opcode,caesar_key).c_str());
+  if(op_code==10){
+      dec_msg.hdr.opcode=op_code;
+      strcpy(dec_msg.ID,decrypt_util(userid,caesar_key).c_str());
+      strcpy(dec_msg.password,decrypt_util(password,caesar_key).c_str());
+      dec_msg.q=atoll(decrypt_util(q,caesar_key).c_str());
+  }
+  else if(enc_msg.hdr.opcode=="30"){
+
+  }
+  else if(enc_msg.hdr.opcode=="50"){
+
+  }
+
+  return dec_msg;
+}
+
+string decrypt_util(string input,ll caesar_key){
+  int n=input.size();
+  string s;
+  s.resize(n,' ');
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<66;j++)
+		{
+			if(input[i]==encoding_scheme[j])
+			{	
+				s[i]=encoding_scheme[(j - caesar_key + 66) % 66];
+				break;
+			}	
+		}
+	}
+	return s;
+}
+
 
 int start_server()
 {
    int sfd; 
    struct sockaddr_in saddr;
    int status;
-
    sfd = socket(AF_INET, SOCK_STREAM, 0);
-   if (sfd == -1) {
+   if(sfd==-1) {
       fprintf(stderr, "Server error: unable to get socket descriptor\n");
       exit(1);
    }
-
    saddr.sin_family = AF_INET; 
    saddr.sin_port = htons(SERVER_PORT);  
    saddr.sin_addr.s_addr = INADDR_ANY;
@@ -78,21 +186,26 @@ int start_server()
 void server_loop(int server_fd){
   char cs_buf[MAX_LEN];
   char sc_buf[MAX_LEN];
-  int cfd;             
   struct sockaddr_in caddr;
   socklen_t size;
   
   
   while(true){
-    cfd = accept(server_fd,(struct sockaddr *)&caddr,&size);
+    cout<<"Ready to accept connections............"<<endl;
+    int cfd = accept(server_fd,(struct sockaddr *)&caddr,&size);
     if (cfd == -1){
       fprintf(stderr, "Server error: unable to accept request\n");
       continue;
     }
     cout<<"Connected with "<<inet_ntoa(caddr.sin_addr)<<endl;
     memset(cs_buf,0,sizeof(cs_buf));
-    recv(cfd, &cs_buf, sizeof(cs_buf),0);
-    //cout<<cs_buf<<endl;
+    int nbytes=recv(cfd, &cs_buf, sizeof(cs_buf),0);
+
+     if (nbytes==0) {
+       fprintf(stderr, "Server error: unable to receive\n");
+       //exit(EXIT_FAILURE);
+       break;
+     }
     memset(sc_buf,0,sizeof(sc_buf));
 
     int i=0,j=0;
@@ -129,47 +242,155 @@ void server_loop(int server_fd){
     cout<<"Public key of server:"<<server_public_key<<endl;
     ll shared_key_server=power(client_public_key,server_private_key,prime);
     cout<<"shared key from server:"<<shared_key_server<<endl;
-    sprintf(cs_buf,"%lld",server_public_key);
-    send(cfd,sc_buf,strlen(sc_buf),0);
+    sprintf(sc_buf,"%lld",server_public_key);
+    int c=send(cfd,sc_buf,strlen(sc_buf),0);
+    if(c>0){
+      cout<<"Message sent"<<endl;
+    }
     cout<<"Key exhange finished"<<endl;
     ll caesar_key=shared_key_server%66;
     cout<<"Caesar key:"<<caesar_key<<endl;
     std::thread t(comm_with_client,cfd,caesar_key);
     t.detach();
-      }
+  }
 }
 
 void comm_with_client(int cfd,ll caesar_key){
   int nbytes;
   while(true){
     
-    Msg send_msg,recv_msg;
-
-    nbytes=recv(cfd,&recv_msg,sizeof(Msg), 0);
-     if (nbytes == -1) {
+    Msg dec_recv_msg,send_msg;
+    EncMsg enc_send_msg,enc_recv_msg;
+    nbytes=recv(cfd,&enc_recv_msg,sizeof(enc_recv_msg),0);
+    cout<<"received encrypted message bytes from client:"<<nbytes<<endl;
+     if (nbytes<=0){
        fprintf(stderr, "Server error: unable to receive\n");
+       //exit(EXIT_FAILURE);
+       break;
      }
+     /* decrypt here  */
+      dec_recv_msg=decrypt_msg(enc_recv_msg,caesar_key);
+      int opcode=dec_recv_msg.hdr.opcode;
 
-     switch(recv_msg.Hdr.opcode){
+      cout<<"opcode received:"<<opcode<<endl;
 
-     case 10:
-       cout<<recv_msg.Msg.ID<<endl;
-       cout<<recv_msg.Msg.password<<endl;
-       cout<<recv_msg.Msg.q<<endl;
-       break;
-     case 30:
-       break;
-     case 50:
-       break;
-       
-     }
+
+      if(opcode==10){
+        cout<<"opcode:"<<dec_recv_msg.hdr.opcode<<endl;
+        string userid(dec_recv_msg.ID);
+        string password(dec_recv_msg.password);
+        ll q=dec_recv_msg.q;
+        cout<<"Decrypted data"<<endl;
+        cout<<"User id:"<<userid<<endl;
+        cout<<"password:"<<password<<endl;
+        cout<<"Prime:"<<q<<endl;
+        bool found=false;
+        for(int i=0;i<users.size();i++){
+          if(userid==users[i].userid){
+            found=true;
+          }
+        }
+        if(found){
+          send_msg.hdr.opcode=20;
+          string rep="NO";
+          strcpy(send_msg.status,rep.c_str());
+          EncMsg enc_send_msg=encrypt_msg(send_msg,caesar_key);
+          int nbytes=send(cfd,&enc_send_msg,sizeof(enc_send_msg),0);
+          if(nbytes<=0){
+            cout<<"Failed to send data to client"<<endl;
+          }
+          else{
+            cout<<"Data sent to client:"<<nbytes<<endl;
+          }
+        }
+        else{
+          ll sum=0;
+          ll salt=rand();
+          for(int i=0;password[i];i++){
+            sum+=(int)password[i];
+          }
+          ll pass_hash=power((sum+q+salt),3,q);
+          user temp;
+          temp.userid=userid;
+          temp.salt=salt;
+          temp.hash=pass_hash;
+          temp.prime=q;
+          users.push_back(temp);
+          send_msg.hdr.opcode=20;
+          string rep="YES";
+          strcpy(send_msg.status,rep.c_str());
+          EncMsg enc_send_msg=encrypt_msg(send_msg,caesar_key);
+          int nbytes=send(cfd,&enc_send_msg,sizeof(enc_send_msg),0);
+          if(nbytes<=0){
+            cout<<"Failed to send data to client"<<endl;
+          }
+          else{
+            cout<<"Data sent to client:"<<nbytes<<endl;
+          }
+        }
+      }
+      else if(opcode==30){
+        cout<<"AUTH REQUEST"<<endl;
+        string userid(dec_recv_msg.ID);
+        string password(dec_recv_msg.password);
+        ll q=dec_recv_msg.q;
+        cout<<"User id:"<<userid<<endl;
+        cout<<"password:"<<password<<endl;
+
+        bool found=false;
+        user temp;
+        for(int i=0;i<users.size();i++){
+          if(userid==users[i].userid){
+            found=true;
+            temp=users[i];
+          }
+        }
+        if(found){
+          cout<<"User does not exist"<<endl;
+          send_msg.hdr.opcode=40;
+          string rep="NO";
+          strcpy(send_msg.status,rep.c_str());
+          EncMsg enc_send_msg=encrypt_msg(send_msg,caesar_key);
+          send(cfd,&enc_send_msg,sizeof(enc_send_msg),0);
+        }
+        else{
+          ll sum=0;
+          ll salt=temp.salt;
+          ll prime=temp.prime;
+          for(int i=0;password[i];i++){
+            sum+=(int)password[i];
+          }
+          ll pass_hash=power((sum+prime+salt),3,prime);
+          if(pass_hash==temp.hash){
+            cout<<"Password matched"<<endl;
+            Msg send_msg;
+            send_msg.hdr.opcode=40;
+            string rep="YES";
+            strcpy(send_msg.status,rep.c_str());
+            EncMsg enc_send_msg=encrypt_msg(send_msg,caesar_key);
+            send(cfd,&enc_send_msg,sizeof(enc_send_msg),0);
+          }
+          else{
+            cout<<"Password not matched"<<endl;
+            Msg send_msg;
+            send_msg.hdr.opcode=40;
+            string rep="NO";
+            strcpy(send_msg.status,rep.c_str());
+            EncMsg enc_send_msg=encrypt_msg(send_msg,caesar_key);
+            send(cfd,&enc_send_msg,sizeof(enc_send_msg),0);
+          }
+        }
+      }
+      else if(opcode==50){
+        
+      }    
   }
 
-  
 }
 
 int main(){
   int server_fd;
+  signal(SIGPIPE, SIG_IGN);
   server_fd = start_server();
   server_loop(server_fd);
   close(server_fd);
